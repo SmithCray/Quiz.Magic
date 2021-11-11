4938bff575e7ee4517c509d6d919a4d624e8f98b
SmithCray <cmsmith004@gmail.com>
2021-11-10 17:19:26 -0700

    structure_frame

---
+++ Assets/js/script.js
@@ -0,0 +1,30 @@
+var timer = 60;
+var displayTimer = document.querySelector("displayTimer");
+
+// function startTimer() {
+//   timer = timer - 1;
+//   setTimeout(startTimer, 1000);
+//   console.log(timer);
+// }
+
+// function startTimer() {
+//   timer = timer - 1;
+//   elem1.innerHTML = "startBtn" + timer + " seconds";
+//   setTimeout(startTimer, 1000);
+//   console.log(time);
+// }
+// document.getElementById("startBtn").addEventListener("click", function () {
+//   var timeleft = 15;
+
+//   var downloadTimer = setInterval(function function1() {
+//     document.getElementById("countdown").innerHTML =
+//       timeleft + " " + "seconds remaining";
+
+//     timeleft -= 1;
+//     if (timeleft <= 0) {
+//       clearInterval(downloadTimer);
+//       document.getElementById("countdown").innerHTML = "Time is up!";
+//     }
+//   }, 1000);
+// });
+// console.log(countdown);
