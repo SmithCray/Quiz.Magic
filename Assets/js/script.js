b5869f5a28463a23edc26f8b9fb374054e8503fa
SmithCray <cmsmith004@gmail.com>
2021-11-11 19:39:44 -0700

    js_edits/troubleShooting/research_functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,43 +1,148 @@
-class Quiz {
-  constructor(questions) {
-    this.score = 0;
-    this.questions = 0;
-    this.questionIndex = 0;
-  }
-  getQuestionIndex() {
-    return this.question[this.questionIndex];
-  }
-  guess(answer) {
-    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
-      this.score++;
-    }
-    this.questionIndex++;
-  }
-  isEnded() {
-    return this.questionIndex === this.questions.length;
-  }
-}
-class Question {
-  constructor(text, choice, answer) {
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
-  }
-  isCorrectAnswer(choice) {
-    return this.answer === choice;
-  }
-}
+var currentQuestion = "#currentQuestion";
+var quizTimer = "#quizTimer";
+var startBtn = "#startBtn";
+var nextBtn = "#nextBtn";
 
-function displayQuestion() {
-  if (Quiz.isEnded()) {
-    showScores();
-  } else {
-    let questionElement = document.getElementById("question");
-    questionElement.innerHTML = quiz.getQuestionIndex().text;
+var questionCounter = 0;
 
-    let choices = quiz.getQuestionIndex().choices;
-  }
-}
+var myQuestions = [
+  {
+    questions: "How many different colors of mana are in Magic?",
+    choices: ["4", "6", "9", "10"],
+    correstAnswer: 1,
+  },
+  {
+    questions: "How many cards are in a startdard deck?",
+    choices: ["40", "45", "55", "60"],
+    correctAnswer: 3,
+  },
+  {
+    questions: "How many cards can you have in your sideboard?",
+    choices: ["12", "15", "17", "20"],
+    correctAnswer: 1,
+  },
+];
+console.log(myQuestions);
+
+document.getElementById("startBtn").onclick = function quizStart(startQuiz) {
+  alert("You have started the quiz!");
+};
+// startQuiz = () => {
+// questionCounter = 0
+// sore =0
+// availableQuestions = [...questions]
+// nextQuestion()
+// }
+
+// getNewQuestion =() => {
+// if(availableQuestions.length === 0 || questionCounter > MAX_QUESTION)
+
+// class Quiz {
+//   constructor(questions) {
+//     this.score = 0;
+//     this.questions = 0;
+//     this.questionIndex = 0;
+//   }
+//   getQuestionIndex() {
+//     return this.questions[this.questionIndex];
+//   }
+//   guess(answer) {
+//     if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+//       this.score++;
+//     }
+//     this.questionIndex++;
+//   }
+//   isEnded() {
+//     return this.questionIndex === this.questions.length;
+//   }
+// }
+// class Question {
+//   constructor(text, choices, answer) {
+//     this.text = text;
+//     this.choices = choices;
+//     this.answer = answer;
+//   }
+//   isCorrectAnswer(choice) {
+//     return this.answer === choice;
+//   }
+// }
+
+// function displayQuestion() {
+//   if (quiz.isEnded()) {
+//     showScores();
+//   } else {
+//     let questionElement = document.getElementById("question");
+//     questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+//     let choices = quiz.getQuestionIndex().choices;
+//     for (let i = 0; i < choices.length; i++) {
+//       let choiceElement = document.getElementById("choice" + i);
+//       choiceElement.innerHtml = choices[i];
+//       guess("btn" + i, choices[i]);
+//     }
+
+//     showProgress();
+//   }
+// }
+
+// function guess(id, guess) {
+//   let button = document.getElementById(id);
+//   button.onclick = function () {
+//     Quiz.guess(guess);
+//     displayQuestion();
+//   };
+// }
+
+// function showProgress() {
+//   let currentQuestionNumber = Quiz.questionIndex + 1;
+//   let progressElement = document.getElementById("progress");
+//   progressElement.innerHTML =
+//     "Question ${currentQuestionNumber} of ${quiz.questions.length}";
+// }
+// ****
+// function showScores() {
+//     let quizEndHTML =
+//         '
+//             <h1>Quiz Completed</h1>
+//             <h2 id="score">You Scored: ${Quiz.score} of ${Quiz.questions.length}</h2><div class="quiz-repeat">
+//                     <a href="index.html">Take Quiz Again</a>
+//                 </div></>
+//         ';
+//         let quizElement = document.getElementById("quiz");
+//         quizElement.innerHTML = quizEndHTML;
+// }
+// ******
+// let questions = [
+//   new Question(
+//     "How mana types of mana are there in Magic?",
+//     ["4", "8", "9", "11"],
+//     "9"
+//   ),
+//   new Question(
+//     "How mana types of mana are there in Magic?",
+//     ["4", "8", "9", "11"],
+//     "9"
+//   ),
+//   new Question(
+//     "How mana types of mana are there in Magic?",
+//     ["4", "8", "9", "11"],
+//     "9"
+//   ),
+//   new Question(
+//     "How mana types of mana are there in Magic?",
+//     ["4", "8", "9", "11"],
+//     "9"
+//   ),
+//   new Question(
+//     "How mana types of mana are there in Magic?",
+//     ["4", "8", "9", "11"],
+//     "9"
+//   ),
+// ];
+
+// let quiz = new Quiz(questions);
+
+// function displayQuestion();
 
 // var timer = 60;
 // var displayTimer = document.querySelector("displayTimer");
